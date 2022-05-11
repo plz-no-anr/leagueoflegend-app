cbce4d6d0fffc8e389d80f71b4e8d15572eb572d
P-SG <psgxxx@naver.com>
2022-05-11 11:40:50 +0900

    파일 삭제

--- data/src/main/java/com/psg/data/utils/ContextManager.kt
+++
@@ -1,9 +0,0 @@
-package com.psg.data.utils
-
-import android.app.Application
-
-object ContextManager:Application() {
-    private val context = applicationContext
-
-    fun getContext() = context
-}
